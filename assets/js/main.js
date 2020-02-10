bbddc9f4f4b4bd6de900da206c4ff4e0164f0d6f
davitgurabanidze <davidgurabanidze@yahoo.com>
2020-02-10 10:53:58 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
   
-// let add = document.getElementsByTagName('svg').firstChild;
-// console.log(add);
-// let bl = add.length;
-// console.log(bl)
-// for (let i = 0; i < bl; i++) {
+let add = document.getElementsByTagName('svg').firstChild;
+console.log(add);
+let bl = add.length;
+console.log(bl)
+for (let i = 0; i < bl; i++) {
 	
-// 	add[i].addEventListener('click', function(e) {
-// 		console.log(i)
-// 		this.style.fill='black';
-// });
-// }
+	add[i].addEventListener('click', function(e) {
+		console.log(i)
+		this.style.fill='black';
+});
+}
